be99a2b83a82ac673be3024ac456338ffad97f3a
studiofoxx <35854919+studiofoxx@users.noreply.github.com>
2022-11-08 21:06:45 -0600

    Add files via upload

---
+++ epic band name generator.py
@@ -0,0 +1,5 @@
+print ('The Most Epic Band Names of All Time!!!')
+
+a = input('Whats your favorite food?\n')
+b = input('whats your favorite color?\n')
+print("the most epic new band is: "+ a+" "+b)
---
+++ variables exercise.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+# a is equal to the input
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#for c in a:
+#        print: b
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
