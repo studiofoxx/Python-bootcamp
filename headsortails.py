63f44c845337228eed86268cfc4125c5e3b84967
studiofoxx <35854919+studiofoxx@users.noreply.github.com>
2022-12-02 13:46:29 -0600

    Add files via upload

---
+++ headsortails.py
@@ -0,0 +1,22 @@
+##heads or tails exercise
+##uses a random generator
+##to determine heads or tails
+import random
+
+print ('what is your move?')
+playerpick = int(input())
+
+#if user_input == 
+heads = 1
+tails = 2
+cpupick = int(random.randint(1,2))
+print ("coin flip lands on:\n", cpupick)
+if cpupick == playerpick:
+    print ("you win!")
+else:
+    print ("you lose")
+
+### if i define rock paper and scissors i can call it throught the project
+## if im trying to define user input 
+## 
+
